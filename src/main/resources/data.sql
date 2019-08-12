351f396a248560ab7dacb791632ead0e45c46f91
Dawit Mebrahtu <dawitaraya7@gmail.com>
2019-08-12 16:48:26 -0500

    updated admin credentials

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,7 @@ INSERT INTO `role` VALUES (1,'ROLE_ADMIN');
 INSERT INTO `role` VALUES (2,'ROLE_SELLER');
 INSERT INTO `role` VALUES (3,'ROLE_BUYER');
 
-INSERT INTO `users`(user_id, enabled, email, password) VALUES (1, true,'dmebrahtu@mum.edu', '$2a$10$Vx/y9T1YimhX58ZcWiCH2.kLcO9X9J1eQXmilfCsNHfzZMtUfjrt2');
+INSERT INTO `users`(user_id, enabled, email, password) VALUES (1, true,'admin', '$2a$10$GrRA7FvSa2ewQhJSsYu8gu493UDQ3hDyxDtwTqOPVLmYHZ6G.DHBu');
 INSERT INTO `user_role`(user_id, role_id) VALUES (1, 1);
 INSERT INTO `user_role`(user_id, role_id) VALUES (1, 2);
 INSERT INTO `user_role`(user_id, role_id) VALUES (1, 3);
